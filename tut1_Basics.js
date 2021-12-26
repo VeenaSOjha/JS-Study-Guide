7bd53a5d3b979059205e15a840822f0b0a576555
veenarojha@gmail.com <veenarojha@gmail.com>
2021-12-26 16:07:28 -0500

    first commit

---
+++ tut1_Basics.js
@@ -0,0 +1,57 @@
+// const myHeading = "JS Basics to Know";
+
+// document.body.innerHTML = myHeading;
+console.log("Notes on basics");
+
+//Click me to display Date and Time
+// function dateFunction() {
+//   document.getElementById("demo1").innerHTML = Date();
+// }
+
+//Change the Html content
+// function changeHtml() {
+//   document.getElementById("demo2").innerHTML =
+//     "This line is coming from Javascript";
+// }
+
+//Change the Html Attribute values
+//Change the value of html attribute
+// function smileyface() {
+//   document.getElementById("face").src =
+//     "https://m.media-amazon.com/images/I/51zLZbEVSTL._AC_SL1200_.jpg";
+//   height = "500";
+//   width = "500";
+// }
+
+// function sadface() {
+//   document.getElementById("face").src =
+//     "https://images-na.ssl-images-amazon.com/images/I/711NDQNFqxL.png";
+//   height = "500";
+//   width = "500";
+// }
+
+//Change the HTML Styles
+// function changeStyle() {
+//   document.getElementById("demo3").style.fontSize = "50px";
+//   document.getElementById("demo3").style.color = "red";
+// }
+
+// //Hide elements
+// function hide() {
+//   document.getElementById("demo4").style.display = "none";
+// }
+
+// //can show elements again
+// function show() {
+//   document.getElementById("demo4").style.display = "block";
+// }
+
+// //JS Statements
+// let x, y, z;
+// var a;
+// var a = 7; //allowed for var
+// var a;
+// console.log(a);
+// x = 5;
+// // let y = 6; //y already declared let so not allowed
+// console.log(x, y, z);
